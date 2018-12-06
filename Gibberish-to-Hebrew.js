879c6e584101eb64528220d3dfaf9f318a9dcc54
Oren Shatz <oren@seoisrael.co.il>
2018-12-06 17:33:49 +0200

    Multiple occurrences
    Update for multiple occurrences (regex instead of regular replace).

--- Gibberish-to-Hebrew.js
+++ Gibberish-to-Hebrew.js
@@ -5,7 +5,7 @@ var Hebrew = "אבגדהוזחטיךכלםמןנסעףפץצקרשת";
 
 var i;
 for (i = 0; i < Gibberish.length; i++) { 
-   res = res.replace(Gibberish.substring(i,i+1), Hebrew.substring(i,i+1));
+   res = res.replace(new RegExp(Gibberish.substring(i,i+1), 'g'), Hebrew.substring(i,i+1));
 }
 
 output = [{text: res}];
